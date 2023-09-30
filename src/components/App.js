15b3c7b25d74eb4937e99ad9d63553c9a917d840
Anthony Simental <anthonysimental12@gmail.com>
2023-09-30 12:40:36 -0700

    lab deliverables completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,27 @@
 // create your App component here
+import React, { useState, useEffect } from "react";
+
+function App() {
+  const [dogImage, setDogImage] = useState([]);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const dogURL = "https://dog.ceo/api/breeds/image/random";
+
+  useEffect(() => {
+    fetch(dogURL)
+      .then((r) => r.json())
+      .then((data) => {
+        setDogImage(data.message);
+        setIsLoaded(true);
+      })
+  }, []);
+
+  if (!isLoaded) return <p>Loading...</p>;
+
+  return (
+    <div>
+      <img src={dogImage} alt="A Random Dog" />
+    </div>
+  )
+}
+
+export default App;
